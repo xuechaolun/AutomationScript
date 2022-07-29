0c30dd61e241ccd888a9a3180aa24ccfcb878069
xuechaolun <56504965+xuechaolun@users.noreply.github.com>
2022-07-29 22:25:06 +0800

    Update merge_same_line.py

--- split_c/merge_same_line.py
+++ split_c/merge_same_line.py
@@ -1,4 +1,3 @@
-import time
 
 def b(f, s):
     f_list = f.split('.')
@@ -9,22 +8,21 @@ def b(f, s):
         flag = 2
     return True if f_list[:flag] == s_list[:flag] else False
 
+
 def main():
-	fr = open('text_c.txt', mode="r", encoding='utf-8')
-	fw = open("result.txt", mode="w", encoding='utf-8')
-	d1 = fr.readline()
-	while d1:
-		d2 = fr.readline()
-		d1_list = d1.split('\t')
-		d2_list = d2.split('\t')
-		if d1_list[2:] == d2_list[2:] and b(d1_list[0], d2_list[1]):
-			d2_list[0] = d1_list[0]
-		else:
-			fw.write(d1)
-		d1 = '\t'.join(d2_list)
-	fr.close()
-	fw.close()
-	print(f"\n执行结果在 result.txt 中\n")
-	
+    with open('text_c.txt', mode="r", encoding='utf-8') as fr:
+        with open("result.txt", mode="w", encoding='utf-8') as fw:
+            d1 = fr.readline()
+            while d1:
+                d2 = fr.readline()
+                d1_list = d1.split('\t')
+                d2_list = d2.split('\t')
+                if d1_list[2:] == d2_list[2:] and b(d1_list[0], d2_list[1]):
+                    d2_list[0] = d1_list[0]
+                else:
+                    fw.write(d1)
+                d1 = '\t'.join(d2_list)
+
+
 if __name__ == '__main__':
-	main()
+    main()
